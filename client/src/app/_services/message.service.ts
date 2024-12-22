85212fb54fd8c6f1bf257ec12eb3121ea1ccbb5a
DawnBreaker <tunganhngo207@gmail.com>
2024-12-22 16:42:17 +0700

    feat: add hub connection to client

--- client/src/app/_services/message.service.ts
+++ client/src/app/_services/message.service.ts
@@ -1,17 +1,46 @@
-import { inject, Injectable, signal } from '@angular/core';
-import { environment } from '../../environments/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { PaginatedResult } from '../_models/pagination';
+import { inject, Injectable, signal } from '@angular/core';
+import {
+  HubConnection,
+  HubConnectionBuilder,
+  HubConnectionState,
+} from '@microsoft/signalr';
+import { environment } from '../../environments/environment.development';
 import { Message } from '../_models/message';
+import { PaginatedResult } from '../_models/pagination';
 import { setPaginatedResponse, setPaginationHeaders } from './paginationHelper';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root',
 })
 export class MessageService {
   baseUrl = environment.apiUrl;
+  hubUrl = environment.hubUrl;
   private http = inject(HttpClient);
+  private hubConnection?: HubConnection;
   paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
+  messageThread = signal<Message[]>([]);
+
+  createHubConnection(user: User, otherUsername: string) {
+    this.hubConnection = new HubConnectionBuilder()
+      .withUrl(`${this.hubUrl}/message?user=${otherUsername}`, {
+        accessTokenFactory: () => user.token,
+      })
+      .withAutomaticReconnect()
+      .build();
+
+    this.hubConnection.start().catch((error) => console.log(error));
+    this.hubConnection.on('ReceiveMessageThread', (messages) => {
+      this.messageThread.set(messages);
+    });
+  }
+
+  stopHubConnection() {
+    if (this.hubConnection?.state == HubConnectionState.Connected) {
+      this.hubConnection.stop().catch((error) => console.log(error));
+    }
+  }
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
     let params = setPaginationHeaders(pageNumber, pageSize);
