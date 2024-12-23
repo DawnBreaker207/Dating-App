6bb518ebf52c0575acbc9d7c2dd45234f2447da7
DawnBreaker <tunganhngo207@gmail.com>
2024-12-23 15:22:36 +0700

    feat: config for deploy production env

---
+++ server/Controllers/FallbackController.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+public class FallbackController : Controller
+{
+  public ActionResult Index()
+  {
+    return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(),
+        "wwwroot", "index.html"), "text/HTML");
+  }
+}
